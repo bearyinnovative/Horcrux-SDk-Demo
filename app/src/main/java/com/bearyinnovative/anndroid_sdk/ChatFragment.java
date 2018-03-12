fdd2122c663b0ed02ec1d819031a0db1ac0b22eb
Nickming <962570483@qq.com>
2018-03-12 18:53:23 +0800

    Revert "add watermark"
    This reverts commit 52d5c64aecb8cc4fa38650c8d46aeea35607f250.

--- app/src/main/java/com/bearyinnovative/anndroid_sdk/ChatFragment.java
+++ app/src/main/java/com/bearyinnovative/anndroid_sdk/ChatFragment.java
@@ -38,6 +38,6 @@ public class ChatFragment extends Fragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        bearyConversationView.initConversationByParams("beary", "Beary Innovative", "15625430452", "abc962570483",true);
+        bearyConversationView.initConversationByParams("beary", "Beary Innovative", "15625430452", "abc962570483");
     }
 }
