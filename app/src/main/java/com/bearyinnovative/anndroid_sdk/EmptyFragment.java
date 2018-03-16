32acf8924e6983ec33597c82b97caa2af9237150
classTC <classtc.cn@gmail.com>
2018-03-16 11:39:01 +0800

    add jump to message activity button

--- app/src/main/java/com/bearyinnovative/anndroid_sdk/EmptyFragment.java
+++ app/src/main/java/com/bearyinnovative/anndroid_sdk/EmptyFragment.java
@@ -8,16 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bearyinnovative.horcrux.ui.util.ActivityUtil;
+
 /**
  * Created by nickming on 05/03/2018.
  */
 
-public class EmptyFragment extends Fragment{
+public class EmptyFragment extends Fragment {
 
     public static EmptyFragment newInstance() {
-        
+
         Bundle args = new Bundle();
-        
+
         EmptyFragment fragment = new EmptyFragment();
         fragment.setArguments(args);
         return fragment;
@@ -26,6 +28,21 @@ public class EmptyFragment extends Fragment{
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_empty,container,false);
+        View view = inflater.inflate(R.layout.fragment_empty, container, false);
+
+        view.findViewById(R.id.bt_start_p2p_chat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityUtil.INSTANCE.startMessagesActivity(getActivity(), "=bwNFB", null);
+            }
+        });
+
+        view.findViewById(R.id.bt_start_group_chat).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityUtil.INSTANCE.startMessagesActivity(getActivity(), "=ccZKs3pUt", null);
+            }
+        });
+        return view;
     }
 }
